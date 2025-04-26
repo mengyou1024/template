4056aa26a493dd14406dc68b7ba6bb62790603d0
mengyou1024 <mengyou1024@foxmail.com>
2025-04-26 19:26:33 +0800

    feat(cmake_vcpkg): 支持生成共享库
    - 在 CMakeLists.txt 中添加了对共享库导出符号的处理
    - 在 add.hpp 中添加了对共享库 API 的定义
    - 修改了预设文件，为共享库生成添加必要的 CMake 变量
    - 优化了 Clang-Tidy 的使用条件

--- cmake_vcpkg/include/add.hpp
+++ cmake_vcpkg/include/add.hpp
@@ -1,6 +1,13 @@
 #pragma once
-
-int Add(int a, int b);
+{% if build_type == "lib" and library_type == "SHARED" %}
+#ifdef {{ project_name_upper }}_EXPORTS
+    #define {{ project_name_upper }}_API __declspec(dllexport)
+#else
+    #define {{ project_name_upper }}_API __declspec(dllimport)
+#endif
+{%- endif %}
+{% if build_type == "lib" and library_type == "SHARED" %}
+{{ project_name_upper }}_API {% endif -%} int Add(int a, int b);
 {% if enable_rust %}
 void HelloRust(void);
 {% endif %}
